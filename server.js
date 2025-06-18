216ab4e2dc66f67e5bb27e8abdc22fd0508095e6
kamalbc27 <kamal.221312@ncit.edu.np>
2025-06-18 22:20:09 +0545

    revise commit

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express= require('express')
 const app= express()
 const ejs=require('ejs')
 const path=require('path')
-const expressLayout=require('express-js-layouts')
+const expressLayout=require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+
 app.get('/',(req,res)=>{
-res.send('hello From server')
+res.render('home')
 })
 // set template engine
 app.use(expressLayout)
-app.use('views','')
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
 app.listen(PORT, ()=>{
 console.log(` listening on port ${PORT}`)
 })
